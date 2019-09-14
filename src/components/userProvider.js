bbd858d716ad1a94ff4a627a87eaf4fc3d8d1317
unknown <nduross1@gmail.com>
2019-09-14 00:15:27 -0700

    haha

--- src/components/userProvider.js
+++
@@ -1,7 +0,0 @@
-import React, { useEffect, useState } from 'react';
-
-const NewContext = React.createContext();
-
-export const UserProvider = NewContext.Provider;
-export const UserConsumer = NewContext.Consumer;
-export default NewContext;
