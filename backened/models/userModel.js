01c210ccd77131386d449631e9546b8dbec6e238
unknown <nduross1@gmail.com>
2019-09-20 09:19:02 -0700

    google auth

--- backened/models/userModel.js
+++ backened/models/userModel.js
@@ -4,20 +4,15 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
 	username: {
 		type: String,
-		required: true,
 		trim: true
 	},
 	password: {
 		type: String,
-		required: true,
 		trim: true
 	},
 	email: {
 		type: String,
-		required: true,
-		trim: true,
-		unique: true,
-		minlength: 7
+		trim: true
 	},
 	terms: {
 		type: Boolean,
