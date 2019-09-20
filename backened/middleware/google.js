daaec0d717665fd8f7759038668706a6f34d1f6c
unknown <nduross1@gmail.com>
2019-09-20 11:50:12 -0700

    google auth

--- backened/middleware/google.js
+++ backened/middleware/google.js
@@ -10,8 +10,7 @@ module.exports = (passport) => {
 			{
 				clientID: process.env.GOOGLE_CLIENT_ID,
 				clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-				callbackURL: 'https://eazibusi.herokuapp.com/auth/google/callback',
-				passReqToCallback: true
+				callbackURL: 'https://eazibusi.herokuapp.com/auth/google/callback'
 			},
 			function(accessToken, refreshToken, profile, done) {
 				console.log(accessToken, profile);
